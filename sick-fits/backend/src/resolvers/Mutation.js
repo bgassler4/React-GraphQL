1b2a505214422901ca8f3634a58e6ee1a74675cf
Brendan Gassler <bgassler4@gmail.com>
2020-07-21 14:45:46 +1000

    First Items creation and prisma flow

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,12 +1,24 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if user is logged in
 
-    //create a dog
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+    //all methods in prisma.graphql are now accessible to us here
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args },
+      },
+      info
+    );
+
+    return item;
   },
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   //create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  //},
 };
 
 module.exports = Mutations;
